1519c1fe694dc3c552968b0936bfdb0c0f9ef235
Davor <davor.komusanac@gmail.com>
2020-07-24 15:30:45 +0200

    refactoring

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,19 +5,3 @@ const kPopupMenuItemStyle = TextStyle(
   fontWeight: FontWeight.w400,
   color: Colors.black,
 );
-
-PopupMenuItem<String> generatePopupMenuItem({String value, String title}) {
-  return PopupMenuItem<String>(
-    value: value,
-    child: Text(title),
-    textStyle: kPopupMenuItemStyle,
-  );
-}
-
-List<PopupMenuItem<String>> choices = [
-  generatePopupMenuItem(value: 'first', title: 'Categories'),
-  generatePopupMenuItem(value: 'second', title: 'Archived Activities'),
-  generatePopupMenuItem(value: 'third', title: 'Backup/Export database'),
-  generatePopupMenuItem(value: 'fourth', title: 'Settings'),
-  generatePopupMenuItem(value: 'fifth', title: 'Upgrade'),
-];
